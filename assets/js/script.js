f00362c3f600983aad454c3fbfa8fba03312626d
Will Papp <wpapp90@gmail.com>
2021-01-12 23:22:42 -0600

    added variables and working timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,47 @@
 // Coding Quiz
 
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+var scoreKeep = document.querySelector(".score");
+// var startButton = document.querySelector(".start-button");
+
+var timer;
+var timerCount = 60
+var score = 0
+
+
+function startQuiz() {
+  timerCount = 60
+}
+
+
+
+
+function startTimer() {
+  timer = setInterval(function() {
+    timerCount--;
+    timerElement.innerHTML = timerCount;
+      if (timerCount === 0) {
+      clearInterval(timer);
+      wrongAnswer();
+    }
+  }, 1000);
+}
+
+
+function wrongAnswer() {
+  score++;
+  setScore();
+}
+
+
+
+function setScore() {
+  score.textContent = score;
+}
+
+
+
+document.getElementById("start-button").addEventListener("click", startTimer);
+
+
